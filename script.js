eda26a79b5c4ff54380eab410db9fc49d90f63dc
florimuzic <149759685+florimuzic@users.noreply.github.com>
2023-12-13 09:48:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+/*
+Designed by: Su
+Original image: https://dribbble.com/shots/3177451-light-switch
+*/
+
+
+let main = document.querySelector("#main");
+let swi = document.querySelector("#switch");
+let g = document.querySelector("#g");
+let button = document.querySelector("#button");
+let tap = document.querySelector("#tap");
+let led = document.querySelector("#led");
+let ledi = document.querySelector("#ledi");
+let shadow = document.querySelector("#shadow");
+
+
+let getSwitch = (event) =>{
+
+    main.classList.toggle("m-night");
+    swi.classList.toggle("s-night");
+
+    g.classList.toggle("g-night");
+    button.classList.toggle("b-night");
+    tap.classList.toggle("t-night");
+    led.classList.toggle("l-night");
+    ledi.classList.toggle("li-night");
+    shadow.classList.toggle("sh-night");
+}
+
+swi.addEventListener("click", getSwitch);
